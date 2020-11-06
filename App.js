55b7204953a569ce2cadf83042791f0c4b139524
Michael David Gill <michaelgill1969@gmail.com>
2020-11-06 16:52:29 -0500

    Get App Building Successfully in Android Emulator
    Leave Firebase Cloud Messaging unimplemented for now.

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 // @flow
 import 'react-native-gesture-handler'
 import * as React from 'react'
-import { NativeModules, Platform, YellowBox } from 'react-native'
+import { NativeModules, Platform, LogBox } from 'react-native'
 import { Provider } from 'react-redux'
 import { initializeStore } from './redux/ActionThunks'
 import { ConfigureStore } from './redux/configureStore'
@@ -9,28 +9,28 @@ import { PersistGate } from 'redux-persist/integration/react'
 import Main from './components/MainComponent'
 import Welcome from './components/WelcomeComponent'
 
-YellowBox.ignoreWarnings(['Setting a timer', 'color was given a value of '])
+LogBox.ignoreLogs(['Setting a timer', 'color was given a value of '])
 
-type Props = {
-  FCMToken: string
-}
+// type Props = {
+//   FCMToken: string
+// }
 
 const { persistor, store } = ConfigureStore()
 
 class App extends React.Component<Props> {
-  componentDidMount () {
-    Platform.OS === 'ios'
-      ? store.dispatch(initializeStore(this.props.FCMToken))
-      : Promise.resolve(NativeModules.FCM.getToken())
-        .then(
-          FCMToken => store.dispatch(initializeStore(FCMToken)),
-          error => {
-            var errorMessage = new Error(error.message)
-            throw errorMessage
-          }
-        )
-        .catch(error => console.log(error.message))
-  }
+  // componentDidMount () {
+  //   Platform.OS === 'ios'
+  //     ? store.dispatch(initializeStore(this.props.FCMToken))
+  //     : Promise.resolve(NativeModules.FCM.getToken())
+  //       .then(
+  //         FCMToken => store.dispatch(initializeStore(FCMToken)),
+  //         error => {
+  //           var errorMessage = new Error(error.message)
+  //           throw errorMessage
+  //         }
+  //       )
+  //       .catch(error => console.log(error.message))
+  // }
 
   render () {
     return (
