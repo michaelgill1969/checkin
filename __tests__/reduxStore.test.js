b9036ed376a469b40aad27b9d78eab39c7317490
Michael David Gill <michaelgill1969@gmail.com>
2020-12-10 13:17:56 -0500

    Mock Redux-Persist Module to Silence Type Error During Testing

--- __tests__/reduxStore.test.js
+++ __tests__/reduxStore.test.js
@@ -1,97 +1,79 @@
-// TODO: Investigate type error in Jest output.
 import { ConfigureStore } from '../redux/configureStore'
+
+jest.mock(
+  'redux-persist',
+  () => {
+    const real = jest.requireActual('redux-persist')
+    return {
+      ...real,
+      persistReducer: jest
+        .fn()
+        .mockImplementation((config, reducers) => reducers)
+    }
+  }
+)
 
 describe(
   'redux store',
   () => {
-    const initialState = ConfigureStore().store.getState()
-    const expectedState = {
-      auth: {
-        _persist: {
-          rehydrated: false,
-          version: -1
-        },
-        errorMessage: '',
-        user: null
-      },
-      buddy: {
-        _persist: {
-          rehydrated: false,
-          version: -1
-        },
-        alertTimes: [],
-        checkinInterval: null,
-        checkinTime: '',
-        email: '',
-        errorMessage: '',
-        isAdded: null,
-        lastAlertTime: '',
-        snooze: 9
-      },
-      device: {
-        _persist: {
-          rehydrated: false,
-          version: -1
-        },
-        errorMessage: '',
-        token: ''
-      },
-      inputs: {
-        _persist: {
-          rehydrated: false,
-          version: -1
-        },
-        alertTimes: [],
-        errorMessage: '',
-        height: 0,
-        showTip: true
-      },
-      listener: {
-        _persist: {
-          rehydrated: false,
-          version: -1
-        },
-        errorMessage: '',
-        interval: 0,
-        listeners: []
-      },
-      timer: {
-        _persist: {
-          rehydrated: false,
-          version: -1
-        },
-        errorMessage: '',
-        interval: 0,
-        timers: []
-      },
-      token: {
-        _persist: {
-          rehydrated: false,
-          version: -1
-        },
-        errorMessage: '',
-        password: '',
-        username: ''
-      },
-      user: {
-        _persist: {
-          rehydrated: false,
-          version: -1
-        },
-        checkinInterval: null,
-        checkinTime: '',
-        errorMessage: '',
-        isSignedIn: null,
-        lastAlertTime: '',
-        longestSnooze: 60,
-        shortestInterval: 1800000,
-        snooze: 9
-      }
-    }
-
     it(
       'returns initial state',
-      async () => await expect(initialState).toEqual(expectedState)
+      async () => {
+        const initialState = await ConfigureStore().store.getState()
+        const expectedState = {
+          auth: {
+            errorMessage: '',
+            user: null
+          },
+          buddy: {
+            alertTimes: [],
+            checkinInterval: null,
+            checkinTime: '',
+            email: '',
+            errorMessage: '',
+            isAdded: null,
+            lastAlertTime: '',
+            snooze: 9
+          },
+          device: {
+            errorMessage: '',
+            token: ''
+          },
+          inputs: {
+            alertTimes: [],
+            errorMessage: '',
+            height: 0,
+            showTip: true
+          },
+          listener: {
+            errorMessage: '',
+            interval: 0,
+            listeners: []
+          },
+          timer: {
+            errorMessage: '',
+            interval: 0,
+            timers: []
+          },
+          token: {
+            errorMessage: '',
+            password: '',
+            username: ''
+          },
+          user: {
+            checkinInterval: null,
+            checkinTime: '',
+            errorMessage: '',
+            isSignedIn: null,
+            lastAlertTime: '',
+            longestSnooze: 60,
+            shortestInterval: 1800000,
+            snooze: 9
+          }
+        }
+
+        expect(initialState).toEqual(expectedState)
+      }
     )
   }
 )
