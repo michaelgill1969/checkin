37e6c770be38c16e331d32e015a3a76619774d3b
Michael David Gill <michaelgill1969@gmail.com>
2020-12-25 13:54:04 -0500

    Continue Action-Creator Tests

---
+++ __tests__/reduxActionThunks.test.js
@@ -0,0 +1,94 @@
+import { ConfigureStore } from '../redux/configureStore'
+import * as ActionThunks from '../redux/ActionThunks'
+
+jest.mock(
+  'redux-persist',
+  () => {
+    const real = jest.requireActual('redux-persist')
+    return {
+      ...real,
+      persistReducer: jest
+        .fn()
+        .mockImplementation((config, reducers) => reducers)
+    }
+  }
+)
+
+describe(
+  'redux store dispatch',
+  () => {
+    it(
+      'does not return null',
+      async () => {
+        const result = ActionThunks.add-buddy('a@a.aa') // Promise.resolve('something')
+
+	expect(result).not.toBe(null)
+      }
+    )
+  }
+)
+
+describe(
+  'redux store',
+  () => {
+    it(
+      'returns initial state',
+      async () => {
+        const initialState = await ConfigureStore().store.getState()
+        const expectedState = {
+          auth: {
+            errorMessage: '',
+            user: null
+          },
+          buddy: {
+            alertTimes: [],
+            checkinInterval: null,
+            checkinTime: '',
+            email: '',
+            errorMessage: '',
+            isAdded: null,
+            lastAlertTime: '',
+            snooze: 9
+          },
+          device: {
+            errorMessage: '',
+            token: ''
+          },
+          inputs: {
+            alertTimes: [],
+            errorMessage: '',
+            height: 0,
+            showTip: true
+          },
+          listener: {
+            errorMessage: '',
+            interval: 0,
+            listeners: []
+          },
+          timer: {
+            errorMessage: '',
+            interval: 0,
+            timers: []
+          },
+          token: {
+            errorMessage: '',
+            password: '',
+            username: ''
+          },
+          user: {
+            checkinInterval: null,
+            checkinTime: '',
+            errorMessage: '',
+            isSignedIn: null,
+            lastAlertTime: '',
+            longestSnooze: 60,
+            shortestInterval: 1800000,
+            snooze: 9
+          }
+        }
+
+        expect(initialState).toEqual(expectedState)
+      }
+    )
+  }
+)
