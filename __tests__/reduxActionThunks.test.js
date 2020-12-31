8005e9b78827dd955a26451c3c69b1a2248ef508
Michael David Gill <michaelgill1969@gmail.com>
2020-12-31 17:17:01 -0500

    Continue Action-Creator Tests

--- __tests__/reduxActionThunks.test.js
+++ __tests__/reduxActionThunks.test.js
@@ -1,6 +1,58 @@
 import { ConfigureStore } from '../redux/configureStore'
 import * as ActionThunks from '../redux/ActionThunks'
 
+const expectedState = {
+  auth: {
+    errorMessage: '',
+    user: null
+  },
+  buddy: {
+    alertTimes: [],
+    checkinInterval: null,
+    checkinTime: '',
+    email: '',
+    errorMessage: '',
+    isAdded: null,
+    lastAlertTime: '',
+    snooze: 9
+  },
+  device: {
+    errorMessage: '',
+    token: ''
+  },
+  inputs: {
+    alertTimes: [],
+    errorMessage: '',
+    height: 0,
+    showTip: true
+  },
+  listener: {
+    errorMessage: '',
+    interval: 0,
+    listeners: []
+  },
+  timer: {
+    errorMessage: '',
+    interval: 0,
+    timers: []
+  },
+  token: {
+    errorMessage: '',
+    password: '',
+    username: ''
+  },
+  user: {
+    checkinInterval: null,
+    checkinTime: '',
+    errorMessage: '',
+    isSignedIn: null,
+    lastAlertTime: '',
+    longestSnooze: 60,
+    shortestInterval: 1800000,
+    snooze: 9
+  }
+}
+
 jest.mock(
   'redux-persist',
   () => {
@@ -17,79 +69,41 @@ jest.mock(
 jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter')
 
 describe(
-  'redux store dispatch',
-  () => {
-    it(
-      'does not return null',
-      async () => {
-        const result = ActionThunks.addBuddy('a@a.aa')
-
-        expect(result).not.toBe(null)
-      }
-    )
-  }
-)
-
-describe(
   'redux store',
   () => {
     it(
       'returns initial state',
       async () => {
         const initialState = await ConfigureStore().store.getState()
-        const expectedState = {
-          auth: {
-            errorMessage: '',
-            user: null
-          },
-          buddy: {
-            alertTimes: [],
-            checkinInterval: null,
-            checkinTime: '',
-            email: '',
-            errorMessage: '',
-            isAdded: null,
-            lastAlertTime: '',
-            snooze: 9
-          },
-          device: {
-            errorMessage: '',
-            token: ''
-          },
-          inputs: {
-            alertTimes: [],
-            errorMessage: '',
-            height: 0,
-            showTip: true
-          },
-          listener: {
-            errorMessage: '',
-            interval: 0,
-            listeners: []
-          },
-          timer: {
-            errorMessage: '',
-            interval: 0,
-            timers: []
-          },
-          token: {
-            errorMessage: '',
-            password: '',
-            username: ''
-          },
-          user: {
-            checkinInterval: null,
-            checkinTime: '',
-            errorMessage: '',
-            isSignedIn: null,
-            lastAlertTime: '',
-            longestSnooze: 60,
-            shortestInterval: 1800000,
-            snooze: 9
-          }
-        }
 
         expect(initialState).toEqual(expectedState)
+      }
+    )
+  }
+)
+
+describe(
+  'add-buddy thunk',
+  () => {
+    it(
+      'updates e-mail in buddy reducer',
+      async () => {
+        const email = 'a@a.aa'
+        const initialState = await ConfigureStore().store.getState()
+
+        await ActionThunks.addBuddy(email)
+
+        expect(
+          {
+            ...initialState,
+            email: email
+          }
+        ).toEqual(
+          {
+            ...expectedState,
+            email: email
+          }
+        )
       }
     )
   }
