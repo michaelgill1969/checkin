632fc9e5c4508f27fc2a1ed1071a30a8b1e7cde6
Michael David Gill <michaelgill1969@gmail.com>
2021-02-09 15:45:31 -0500

    Continue Action-Creator Tests
    Improve object modification.

--- __mocks__/@react-native-firebase/firestore.js
+++ __mocks__/@react-native-firebase/firestore.js
@@ -1,14 +1,14 @@
 const db = jest.createMockFromModule('@react-native-firebase/firestore')
 
-// TODO: Document data should be a real store.
-let documentData = { checkinTime: '1970-01-01T00:00:00.000Z' }
+let documentData = {}
 const documentSnapshot = {
-  // simulate firestore get doc.data() function
   data: () => documentData,
   exists: typeof documentData !== 'undefined' && documentData !== null
 }
 const get = jest.fn(() => Promise.resolve(documentSnapshot))
-const set = jest.fn(data => Promise.resolve(documentData = data))
+const set = jest.fn(
+  data => Promise.resolve(documentData = { ...documentData, data })
+)
 const doc = jest.fn(() => { return { set, get } })
 const collection = jest.fn(() => { return { doc } })
 
